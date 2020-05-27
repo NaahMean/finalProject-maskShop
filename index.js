a19160c85f5566f1252dec12b678cc598e812328
nsunam <nsunam@gmail.com>
2020-05-27 16:11:15 -0500

    popimages and slider

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function myFunction() {
 
 window.onclick = function(event) {
   if (!event.target.matches('.dropbutton')) {
-    var dropdowns = document.getElementsByClassName("Gallery");
+    var dropdowns = document.getElementsByClassName("gallery");
     for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
@@ -16,3 +16,54 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+
+// to implement sliders
+
+var images = [
+    'mask1.jpg',
+    'mask2.jpg',
+    'mask3.png',
+    'mask4.jpg',
+    'mask5.jpg', 
+    'mask6.jpg',
+    'mask7.jpg',
+];
+var num = 0;
+function next() {
+    var slider = document.getElementById('slider');
+    num++;
+    if(num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
+function prev() {
+    var slider = document.getElementById('slider');
+    num--;
+    if(num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
+}
+
+/* implement pop up image */
+
+function showMyImage() {
+    var img = document.getElementById('image-holder');
+    img.src = 'mask1.jpg';
+    img.style.display = 'block';
+  }
+
+  // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  img.style.display = none;
+}
+
+
+/* implement pop up image */
+
