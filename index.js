55f25421fbe24605cdd674aad19aa3c8d840cc98
nsunam <nsunam@gmail.com>
2020-05-27 19:56:42 -0500

    simplefinal

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ window.onclick = function(event) {
   }
 }
 
-
-
 // to implement sliders
 
 var images = [
@@ -49,21 +47,23 @@ function prev() {
 }
 
 /* implement pop up image */
-/*
-function showMyImage() {
-    var img = document.getElementById('image-holder');
-    img.src = 'mask1.jpg';
-    img.style.display = 'block';
-  }
+// Get the modal
+var modal = document.getElementById("myModal");
 
-  // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  img.style.display = none;
+// Get the image and insert it inside the modal
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
+// closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-/* implement pop up image */
-
+// (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
